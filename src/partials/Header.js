1128eed32283912d6cbc3fd459f0c9bf10525db2
Arsen <arsen.zuna01@gmail.com>
2024-03-28 06:56:09 +0100

    Fix rel=noreferrer

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 			<div className="d-flex justify-content-between">
 				<h3 className="text-white" style={{paddingLeft: "25px"}}>AZ Portofolio</h3>
 				<div>
-					<a target="_blank" href="https://www.linkedin.com/in/arsen-zuna-5a14a218b/"><FontAwesomeIcon className="m-1 text-white" icon={faLinkedin} style={{fontSize: "30px", paddingRight: "25px"}}/></a>
+					<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/arsen-zuna-5a14a218b/"><FontAwesomeIcon className="m-1 text-white" icon={faLinkedin} style={{fontSize: "30px", paddingRight: "25px"}}/></a>
 				</div>
 			</div>
 			<hr className="text-white"/>
