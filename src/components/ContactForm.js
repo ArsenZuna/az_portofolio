dedaab3b3eebe9f40ed10e5b9cb55c9c6ab3d1cd
Arsen <arsen.zuna01@gmail.com>
2024-03-29 03:31:51 +0100

    fix email bugs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,7 +18,7 @@ const ContactForm = () => {
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		emailjs.sendForm('arsenzuna01', 'email_template1',  e.target, {
+		emailjs.sendForm('arsenzuna01', 'email_template1',  form.current, {
 				publicKey: 'HRZxFHbB071q3qEwo', templateParams,
 			})
 			.then(
